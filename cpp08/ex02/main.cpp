f648ba80070d7f43cf28a81361d62384ae6d5785
NBK-01 <nayefkanaan03@gmail.com>
2025-06-17 16:22:04 +0300

    08 - here we go

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,6 +1,28 @@
-#include "../headers/MutantStack.h"
+#include "./headers/MutantStack.h"
 
 int main()
 {
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
 
+
+	mstack.pop();
+	std::cout << "mstack.top() = " << mstack.top() << std::endl;
+	std::cout << "mstack.size() = " << mstack.size() << std::endl;
+	{
+		MutantStack<int>::iterator it = mstack.begin();
+		MutantStack<int>::iterator ite = mstack.end();
+
+		++it;
+		--it;
+		while (it != ite)
+		{
+			std::cout << *it << std::endl;
+			++it;
+		}
+	}
 }
